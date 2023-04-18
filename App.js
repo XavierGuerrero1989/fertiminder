4a9352d256427ee80d5c6f248447fd110ee065c9
Xavier Guerrero <xavierignacioguerrero@gmail.com>
2023-04-17 22:29:50 -0300

    clase 3 primeras cosas en pantalla. estilos y componentes basicos

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Esta va a ser mi primera App en Android!!!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Enter your task' style={styles.input} />
+        <Button title='Add' color="#52528C"/>
+      </View>
+      <View style={styles.secondContainer}>
+        <Text style={styles.taskList}>Tarea 1</Text>
+        <Text style={styles.taskList}>Tarea 2</Text>
+        <Text style={styles.taskList}>Tarea 3</Text>
+        <Text style={styles.taskList}>Tarea 4</Text>
+      </View>
+      
     </View>
   );
 }
@@ -14,7 +22,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginHorizontal: 20,
   },
+  inputContainer: {
+    flexDirection: 'row',
+    marginTop: 60,
+    alignItems: 'center',
+    justifyContent: "space-between",
+  },
+  input: {
+    width: '80%',
+    borderBottomWidth: 1,
+    borderColor: "#52528C",
+    marginRight: 15,
+    color: "#212121"
+  },
+  secondContainer: {
+    borderWidth: 2,
+    borderColor: '#52528C',
+    marginTop: 20,
+  },
+  taskList: {
+    padding: 20,
+    borderWidth: 1,
+    borderColor: '#52528C',
+    margin: 10
+  }
 });
